336299ba1591c45f5afb5d195c31951fc9b5ceba
Sahil <sahildua2305@gmail.com>
2014-06-28 03:53:05 +0530

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
 
 function extractDataFromHistory(){
-	chrome.history.search({"text": "google"}, function(){
-		document.getElementById('history').html('<p>Sahil</p>');
+	//document.getElementById('history').innerHTML = "hi";
+	chrome.history.search({text: "", maxResults: 10}, function(historyItems){
+		var data = '';
+		for(var i=0; i<historyItems.length; i++){
+			if(historyItems[i].url.indexOf('google')>-1){
+				data += historyItems[i].url;
+				//data += historyItems[i].url.indexOf('google');
+				data += "<br>";
+			}
+		}
+		document.getElementById('history').innerHTML = data;
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function(){
 	extractDataFromHistory();
-}
+});
