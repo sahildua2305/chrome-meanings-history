28ac510a7d758165df9382b782d7035e831fcebd
Sahil <sahildua2305@gmail.com>
2014-06-30 18:57:18 +0530

    Rectified RegExp pattern and css

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,23 @@
-function gup (string,name) {
-	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-	var regexS = "[\\?&]" + name + "=([^&#]*)";
+function regexQuery(string) {
+	var regexS = "google.+q=(.+?)&";
 	var regex = new RegExp(regexS);
 	var results = regex.exec(string);
-	if (results == null) {
-		return '.';
+	if (results == null) 
+		return false;
 	else
 		return results[1];
 }
 
 function extractDataFromHistory(){
 	//document.getElementById('history').innerHTML = "hi";
-	chrome.history.search({text: "", maxResults: 1000}, function(historyItems){
+	chrome.history.search({text: "", maxResults: 100000}, function(historyItems){
 		var data = '';
 		for(var i=0; i<historyItems.length; i++){
 			var url = historyItems[i].url;
-			if(url.indexOf('google')>-1 && url.search('q=')){
-				//data += historyItems[i].url.indexOf('google');
-				data += gup(url, 'q');
+			if(url.indexOf('google')>-1 && url.indexOf('q=')>-1 && regexQuery(url) && regexQuery(url).indexOf('&')!=0){
+				data += regexQuery(url).replace(/\+/g, ' ');
 				data += "<br>";
+				//data += url;
 				data += "<br>";
 			}
 		}
